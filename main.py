1dd6a1c343af933a6f5386d931b112fa0cbc5db1
Phan <manh.phan@makerverse.com>
2025-07-15 12:15:19 +0200

    Code 15.07.2025

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
 import time
 
+from pathlib import Path
 from src.data_loader import *
 from src.training import *
 from src.evaluation import *
 from src.visualization import *
 
 if __name__ == '__main__':
-    pass
+    total = 0
+    for file in os.listdir(r"C:\step_files"):
+        if file.endswith(".STP"):
+            total += 1
+            print(file)
+    print("Total files: ", total)
 
 
